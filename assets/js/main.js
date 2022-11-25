2e9cb858006ca445276b5829df45b28b0041d1db
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2022-11-24 21:34:50 -0500

    toggle mob nav bar on hash links

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,15 +5,34 @@ const mobileNavShowEl = document.querySelector('.mobile-nav-show');
 const mobileNavHideEl = document.querySelector('.mobile-nav-hide');
 console.log(document.querySelectorAll('.mobile-nav-toggle'));
 const mobileNavBtnEl = document.querySelectorAll('.mobile-nav-toggle');
-console.log('ok');
 console.log(mobileNavBtnEl);
 mobileNavBtnEl.forEach(element => {
     console.log(element);
     element.addEventListener('click', function () {
-        console.log('button clicked');
-        mobileNavShowEl.classList.toggle('d-none');
-        mobileNavHideEl.classList.toggle('d-none');
-        document.querySelector('body').classList.toggle('mobile-nav-active');
+        mobileNavToogle();
+
     })
 });
-console.log(hello);
+function mobileNavToogle() {
+    mobileNavShowEl.classList.toggle('d-none');
+    mobileNavHideEl.classList.toggle('d-none');
+    // apply a screen that prevent scrolling and clicking on the body
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+}
+// hide mobile nav on same-page/hash links
+document.querySelectorAll('.navbar a').forEach(navbarlink => {
+    // 1.iterate through all navbar links
+    //2. if a link doesn't have a hash exit the function and check the next one.
+    // 3. if you find a hash check if there is an element that has that hash 
+    // 4. if you don't find it break 
+    // 5. if you find it add a click event listner to it. when clicked check if mobile-nav-active is on and do the same you do when you change page. toggle the nav hide show button and remove the mobile-nav-active screen
+    if (!navbarlink.hash) return;
+    let section = document.querySelector(navbarlink.hash);
+    if (!section) return;
+    navbarlink.addEventListener('click', () => {
+        if (document.querySelector('.mobile-nav-active')) {
+            mobileNavToogle();
+        }
+    })
+
+})
